6c8a5a45f74f7b23b46307bcd200dbd2293aded5
rivizoft <shirko49@mail.ru>
2019-09-23 15:26:14 +0500

    adde list of commands

--- src/Bot.java
+++ src/Bot.java
@@ -12,19 +12,18 @@ public class Bot {
 
         Group group = new Group(186802716, "8e404285f78a50a5261221766acb4bf06e62f0a8fb971569b9aad2ca31e7ab875e6a521be98b98451fe81");
         group.enableTyping(true);
-        group.onSimpleTextMessage(message ->
+        group.onCommand("/help", message ->
                 new Message()
                         .from(group)
-                        .to(message.getFlags())
-                        .text("Что-то скучновато буковки читать. Картинку кинь лучше.")
-                        .send()
-        );
-
-        group.onCommand("/start", message ->
-                new Message()
-                        .from(group)
-                        .to(message.getRandomId())
-                        .text("Привет, я бот староста")
+                        .to(message.authorId())
+                        .text("Привет, меня зовут Пятница, я ваш Бот Староста\n" +
+                                "Я могу выполнять различные функции: \n" +
+                                "/timetable <дата> (вывод расписания на день)\n" +
+                                "/num (четность недели)\n" +
+                                "/homework <дата> (вывод ДЗ на дату)\n" +
+                                "/deadlines (задачи)\n" +
+                                "/attend (отметить посещаемость, доступно старосте)\n" +
+                                "/documents (присылаю доступные учебники/документы)\n")
                         .send());
     }
 }
